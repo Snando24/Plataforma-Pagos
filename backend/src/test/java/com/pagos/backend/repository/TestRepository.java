3c75287bedb0efa9e6e686b9f1d93ccba722be52
Jr. SoftNanTec <nandno1408@gmail.com>
2025-04-05 20:53:40 -0500

    Eliminación de pruebas Unitarias

--- backend/src/test/java/com/pagos/backend/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pagos.backend.repository;
-
-import com.pagos.backend.model.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
